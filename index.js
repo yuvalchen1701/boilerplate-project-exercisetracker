3af187f39a7ba898447fa0b0764827c4f319f4d6
Yuval Chen <118536986+yuvalchen1701@users.noreply.github.com>
2024-11-28 18:11:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const ExerciseSchema = new Schema({
   duration: Number,
   date: Date,
 });
+const Exercise = mongoose.model("Exercise", ExerciseSchema);
 
 app.use(cors())
 app.use(express.static('public'))
@@ -26,8 +27,95 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-app.post('/api/users', (req, res) => {
-  res.send(req.body.username);
+app.get('/api/users', async (req, res) => {
+  const users =  await User.find({}).select("_id username");
+  if(!users) {
+    res.send('No users');
+  } else {
+    res.send(users);
+  }
+});
+
+app.post('/api/users', async(req, res) => {
+ const userObj = new User({
+    username: req.body.username
+  });
+
+  try{
+    const user = await userObj.save();
+    res.json(user);
+  } catch(err) {
+    console.log(err);
+  }
+});
+
+app.post('/api/users/:_id/exercises', async(req, res) => {
+  const id = req.params._id;
+  const {description, duration, date} = req.body;
+
+  try {
+    const user = await User.findById(id);
+    if(!user) {
+      console.log('Could not find user');
+    } else {
+      const exerciseObj = new Exercise({
+        user_id: user._id,
+        description,
+        duration,
+        date: date ? new Date(date) : new Date()
+      });
+      const exercise = await exerciseObj.save();
+
+      res.json({
+        id: user._id,
+        username: user.username,
+        description: exercise.description,
+        duration: exercise.duration,
+        date: new Date(exercise.date).toDateString()
+      });
+    }
+  } catch(err) {
+    console.log(err);
+    res.send("There was an error saving the exercise");
+  }    
+});
+
+app.get('/api/users/:_id/logs', async (req, res) => {
+  const { from, to, limit } = req.query;
+  const id = req.params._id;
+  const user = await User.findById(id);
+
+  if(!user) {
+    res.send("Could not find user");
+    return;
+  } 
+  let dateObj = {};
+  if(from) {
+    dateObj['$gte'] = new Date(from);
+  }
+  if(to) {
+    dateObj['$lte'] = new Date(to);
+  }
+  let filter = {
+    user_id: id
+  }
+  if(from||to) {
+    filter.date = dateObj;
+  }
+
+  const exercise = await Exercise.find(filter).limit(+limit ?? 500);
+  const log = exercise.map(e => ({
+    description: e.description,
+    duration: e.duration,
+    date: e.date.toDateString()
+  }));
+
+  res.json({
+    username: user.username,
+    count: exercise.length,
+    _id: user._id,
+    log
+  });
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
